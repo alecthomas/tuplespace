4a8352b73b8167b8fdd3e0ca7368963818c6dd7b
Alec Thomas <alec@swapoff.org>
2013-12-17 16:54:28 -0500

    Bump Python version.

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ setup(
     name='tuplespace',
     url='http://github.com/alecthomas/tuplespace',
     download_url='http://github.com/alecthomas/tuplespace',
-    version='0.1.0',
+    version='0.2.0',
     description='Python client for a Tuple Space service.',
     license='BSD',
     platforms=['any'],
